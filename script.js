1c8583d94b78a2b88afb9d4c14f10947c4d0e226
joanatas <joanatasfernandesti@gmail.com>
2025-05-29 21:11:59 -0300

    Alterando efeito das fotos

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function startImageRotation(container) {
+    const images = container.querySelectorAll("img");
+    let index = 0;
+
+    images.forEach(img => img.classList.remove("active"));
+    images[0].classList.add("active");
+
+    container._currentIndex = 0;
+
+    if (container._intervalId) return;
+
+    container._intervalId = setInterval(() => {
+        images[container._currentIndex].classList.remove("active");
+        container._currentIndex = (container._currentIndex + 1) % images.length;
+        images[container._currentIndex].classList.add("active");
+    }, 1000);
+}
+
+function stopImageRotation(container) {
+    const images = container.querySelectorAll("img");
+
+    clearInterval(container._intervalId);
+    container._intervalId = null;
+
+    images.forEach(img => img.classList.remove("active"));
+    if (images.length > 0) {
+        images[0].classList.add("active");
+        container._currentIndex = 0;
+    }
+}
+
